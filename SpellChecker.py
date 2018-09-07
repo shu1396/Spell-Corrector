f75f05a41b5200b71d6c7cd1c807fc40d597e762
shu1396 <1396shubham@gmail.com>
2018-09-07 21:50:59 +0530

    Update SpellChecker.py

--- SpellChecker.py
+++ SpellChecker.py
@@ -4,7 +4,7 @@ from collections import Counter
 
 def words(text): return re.findall(r'\w+', text.lower())
 
-WORDS = Counter(words(open('big.txt').read()))
+WORDS = Counter(words(open('dictionary.txt').read()))
 
 def P(word, N=sum(WORDS.values())): 
     "Probability"
